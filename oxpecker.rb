09caa942d16b4588b07c17f9daf791d267135fbc
Jiahua Li <59403109+jiahualihuanahuan@users.noreply.github.com>
2026-09-10 12:48:19 -0400

    Rename MacMemoryCleaner to Oxpecker

--- oxpecker.rb
+++ oxpecker.rb
@@ -1,22 +1,22 @@
 class MacMemoryCleaner < Formula
   desc "CLI tool to find and kill unused background processes on macOS"
-  homepage "https://github.com/jiahualihuanahuan/mac-memory-cleaner"
+  homepage "https://github.com/jiahualihuanahuan/oxpecker"
   
   # The URL to the tar.gz you uploaded to your GitHub Releases
-  url "https://github.com/jiahualihuanahuan/mac-memory-cleaner/releases/download/v1.0.0/mac_memory_cleaner.tar.gz"
+  url "https://github.com/jiahualihuanahuan/oxpecker/releases/download/v1.0.0/oxpecker.tar.gz"
   
   # The SHA-256 hash you generated in the previous steps
-  sha256 "3417e2cbcbf800880eb103c05cecc768a18f1f8592c440ee377e35198487834b"
+  sha256 "b196e2345cf9d07a79576619673d6f215f6dce5582ae2ec68a4aaa8f5ccbb3ba"
   
   version "1.0.0"
 
   def install
     # Installs 'mac_cleaner' from the tarball but renames the command to 'mac_memory_cleaner'
-    bin.install "mac_memory_cleaner"
+    bin.install "oxpecker"
   end
 
   test do
     # A simple test to ensure the tool runs
-    system "#{bin}/mac_memory_cleaner", "--help"
+    system "#{bin}/oxpecker", "--help"
   end
 end
